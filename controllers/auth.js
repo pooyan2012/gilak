46788b4ba98e10287db15b469eee1cb8965e5737
pooyan <permangenat_pooyan@yahoo.com>
2020-07-28 15:39:56 +0430

    auth & admin middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -57,6 +57,27 @@ exports.signout = (req, res) => {
 
 exports.requireSignin = expressJwt({
   secret: process.env.JWT_SECRET,
-  algorithms: ["HS256"],//RS256 //HS256
+  algorithms: ["HS256"], //RS256 //HS256
   userProperty: "auth",
 });
+
+exports.isAuth = (req, res, next) => {
+  let user = req.profile && req.auth && req.profile._id == req.auth._id;
+  if (!user) {
+    return res.status(403).json({
+      error: "access denied",
+    });
+  }
+
+  next();
+};
+
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      error: "Admin resource! Access denied",
+    });
+  }
+
+  next();
+};
