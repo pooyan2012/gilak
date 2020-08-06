1cf994362634ab537c5c711b453b3bbec2905417
pooyan <permangenat_pooyan@yahoo.com>
2020-08-06 09:31:10 +0430

    worked on product

--- controllers/product.js
+++ controllers/product.js
@@ -4,18 +4,54 @@ const fs = require("fs");
 const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+exports.productById = (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if (err || !product) {
+      return res.status(400).json({
+        error: "محصولی یافت نشد",
+      });
+    }
+    req.product = product;
+    next();
+  });
+};
+
+exports.read = (req, res) => {};
+
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
   form.parse(req, (err, fields, files) => {
     if (err) {
       return res.status(400).json({
-        error: "Image could not be uploaded",
+        error: "عکس قابل آپلود نیست",
       });
     }
+
+    //check for all fields
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "تمام فیلد ها باید پر شوند",
+      });
+    }
+
     let product = new Product(fields);
 
     if (files.photo) {
+      if (files.photo.size > 1000000 /* 1mb */) {
+        return res.status(400).json({
+          error: "Image should be less than 1mb",
+        });
+      }
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
