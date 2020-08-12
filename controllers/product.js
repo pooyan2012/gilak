d0045bfe6de349c79ca616c20da1ba69613b6eca
pooyan <permangenat_pooyan@yahoo.com>
2020-08-12 17:13:23 +0430

    product read,delete update + category read,delete,update & list all

--- controllers/product.js
+++ controllers/product.js
@@ -16,7 +16,10 @@ exports.productById = (req, res, next, id) => {
   });
 };
 
-exports.read = (req, res) => {};
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
 
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
@@ -45,6 +48,70 @@ exports.create = (req, res) => {
     }
 
     let product = new Product(fields);
+
+    if (files.photo) {
+      if (files.photo.size > 1000000 /* 1mb */) {
+        return res.status(400).json({
+          error: "Image should be less than 1mb",
+        });
+      }
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      res.json(result);
+    });
+  });
+};
+
+exports.remove = (req, res) => {
+  let product = req.product;
+  product.remove((err, deletedProduct) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json({
+      deletedProduct,
+      message: "Product deleted successfully",
+    });
+  });
+};
+
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "عکس قابل آپلود نیست",
+      });
+    }
+
+    //check for all fields
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "تمام فیلد ها باید پر شوند",
+      });
+    }
+
+    let product = req.product;
+    product = _.extend(product, fields);
 
     if (files.photo) {
       if (files.photo.size > 1000000 /* 1mb */) {
